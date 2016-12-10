d91b20c082c2338e7d898a84d76bce50e3d1bfe5
Zermina Ejaz <N00831354@students.ncc.edu>
2016-12-09 20:44:48 -0500

    Update camera.py
    December 9

--- camera.py
+++ camera.py
@@ -1,6 +1,8 @@
 from picamera import PiCamera
 from time import sleep
 import time
+from subprocess import call
+import os
 
 class WicCamera:
 
@@ -15,7 +17,7 @@ class WicCamera:
         print("BG take pic")
 
         self.camera.annotate_text = 'Get ready!'
-        
+        pictures = [None]*4
         for x in range(0,4):
             #self.camera.start_preview(fullscreen=False, window=(800, 300, 300, 300))
             
@@ -24,14 +26,16 @@ class WicCamera:
             # annotation otherwise can't tell when pic is taken
             self.camera.annotate_text = 'Taking pic ' + str(x+1)
             self.camera.annotate_text = '' # take off annotation right before
-                                           # taking pic so its not in image 
-            self.camera.capture(self.folder + time.strftime("%Y-%m-%d-%H:%M:%S") + '.jpg')
+                                           # taking pic so its not in image
+            pictures[x] = self.folder + time.strftime("%Y-%m-%d-%H:%M:%S") + '.jpg'                               
+            self.camera.capture(pictures[x])
             self.camera.annotate_text = 'Taking pic ' + str(x+1)
             #self.camera.stop_preview()
 
         ##self.camera.close()
         print("end of take pic")
         sleep(1)
+        os.system("montage " + pictures[0] + " " + pictures[1] + " " +  pictures[2] + " " +  pictures[3] + " " + "-geometry +2+2" + " -quality 100" + " montage.jpg")
         self.camera.annotate_text = 'Done Taking pics'
 
        
@@ -41,39 +45,44 @@ class WicCamera:
         self.camera.image_effect = 'cartoon' 
         print("BG of take Cartoon")
         self.camera.annotate_text = 'Get ready!'
+
+        pictures = [None]*4
+        print ("len " + str(len(pictures)))
         
         for x in range(0,4):
             ##camera.start_preview()
             sleep(2)
-            self.camera.capture(self.folder + time.strftime("%Y-%m-%d-%H:%M:%S") + '.jpg')
             self.camera.annotate_text = 'Taking pic ' + str(x+1)
             self.camera.annotate_text = '' # take off annotation right before
-                                           # taking pic so its not in image 
-            self.camera.capture(self.folder + time.strftime("%Y-%m-%d-%H:%M:%S") + '.jpg')
+                                           # taking pic so its not in image
+            pictures[x] = self.folder + time.strftime("%Y-%m-%d-%H:%M:%S") + '.jpg'
+            self.camera.capture(pictures[x])
+            print(pictures[x])
             self.camera.annotate_text = 'Taking pic ' + str(x+1)
             ##camera.stop_preview()
 
         ##self.camera.close()
         print("end of take Cartoon")
         self.camera.annotate_text = 'Done Taking pics'
-        
+       ## call(["montage", pictures[0], pictures[1], pictures[2], pictures[3], "-geometry +2+2", "-quality 100", "montage.jpg"])
+        os.system("montage " + pictures[0] + " " + pictures[1] + " " +  pictures[2] + " " +  pictures[3] + " " + "-geometry +2+2" + " -quality 100" + " montage.jpg")
     def takePastel(self):
 
         self.camera.image_effect = 'pastel' 
         self.camera.annotate_text = 'Get ready!'
-        
+        pictures = [None]*4
         for x in range(0,4):
             ##camera.start_preview()
             sleep(2)
-            self.camera.capture(self.folder + time.strftime("%Y-%m-%d-%H:%M:%S") + '.jpg')
             self.camera.annotate_text = 'Taking pic ' + str(x+1)
             self.camera.annotate_text = '' # take off annotation right before
-                                           # taking pic so its not in image 
-            self.camera.capture(self.folder + time.strftime("%Y-%m-%d-%H:%M:%S") + '.jpg')
+                                           # taking pic so its not in image
+            pictures[x] = self.folder + time.strftime("%Y-%m-%d-%H:%M:%S") + '.jpg'                               
+            self.camera.capture(pictures[x])
             self.camera.annotate_text = 'Taking pic ' + str(x+1)
             ##camera.stop_preview()
-
-        ##self.camera.close()
+        os.system("montage " + pictures[0] + " " + pictures[1] + " " +  pictures[2] + " " +  pictures[3] + " " + "-geometry +2+2" + " -quality 100" + " montage.jpg")
+         ##self.camera.close()
         self.camera.annotate_text = 'Done Taking pics'
         
     def preview(self):
